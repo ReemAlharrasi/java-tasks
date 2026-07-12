206b932b91f8207000f9a9db0f4281bc02f82b1c
unknown <reemalharrasi00@gmail.com>
2026-07-12 16:30:44 +0400

    update

---
+++ java-task-313/src/main/java/RectangleCalculator.java
@@ -0,0 +1,10 @@
+public class RectangleCalculator {
+    int width;
+    int height;
+
+    public RectangleCalculator(int w, int h){
+        width=w;
+        height=h;
+    }
+
+}
---
+++ java-task-313/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    int length;
+    int width;
+    int area;
+    int perimeter;
+    static void main() {
+
+        }
+    }
+}
