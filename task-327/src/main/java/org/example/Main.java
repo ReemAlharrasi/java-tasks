bb197696349199d4a6b6e6f85508008ff428c8a4
unknown <reemalharrasi00@gmail.com>
2026-07-14 12:02:53 +0400

    update

---
+++ task-327/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        //initialize variables
+        int originalNumber = 153;
+
+        // create object
+        ArmstrongNumberChecker checker = new ArmstrongNumberChecker();
+
+        //call method
+        checker.main(originalNumber);
+    }
+}
