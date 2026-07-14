83f6cde787f0e1d347b10eff95ca37112aa07101
unknown <reemalharrasi00@gmail.com>
2026-07-14 09:47:58 +0400

    update

---
+++ task-319/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        //initialize variables
+        int number=1221;
+
+        //initialize object
+        PalindromeChecker checker = new PalindromeChecker(number);
+
+        //call object method
+        checker.main();
+    }
+}
