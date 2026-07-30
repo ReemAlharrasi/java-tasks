9940b77a01689618a28b588461277530ae420a17
ReemAlharrasi <re2ali5em@gmail.com>
2026-07-30 13:13:18 +0400

    solved 357

--- java-352-363/src/main/java/task357/StudentGradesMap.java
+++ java-352-363/src/main/java/task357/StudentGradesMap.java
@@ -5,38 +5,73 @@ import java.util.Scanner;
 
 public class StudentGradesMap {
     //declare variables
-    HashMap<Integer, Double> studentIdGrades=new HashMap<>();
+    HashMap<Integer, Double> studentIdGrades = new HashMap<>();
     Scanner in = new Scanner(System.in);
-    int size,id;
-    double grade;
-
+    int size, id;
+    double grade, totalGrades, average;
+    String classification;
     //main function
-    void main(){
+    void main() {
         //Prompt the user to enter the number of students
-        System.out.print("enter the number of employees: ");
+        System.out.print("enter the number of Students: ");
         size = in.nextInt();
 
         //validate input
-        if (size <= 0){
+        if (size <= 0) {
             System.out.println("Invalid number of students.");
-        }
-        else{
+        } else {
             //if size is valid
             System.out.println("Input -----");
             for (int i = 0; i < size; i++) {
                 //read id
-                System.out.print(i+" - enter student id: ");
+                System.out.print(i + " - enter student id: ");
                 id = in.nextInt();
                 //add to set and ensure there is no duplicate
-                if (studentIdGrades.containsKey(id)){
+                if (studentIdGrades.containsKey(id)) {
                     System.out.println("Student ID already exists. Record not added..");
-                }else{
+                } else {
                     System.out.print("    enter student grade: ");
                     grade = in.nextFloat();
-                    studentIdGrades.put(id,grade);
+                    while (grade>100 || grade<0){
+                        System.out.print("    Invalid grade. enter student grade: ");
+                        grade = in.nextFloat();
+                    }
                 }
+                studentIdGrades.put(id, grade);
             }
 
+            //update grade
+            System.out.print("\nUpdate grade -----\nEnter student id: ");
+            id = in.nextInt();
+            if (!studentIdGrades.containsKey(id)) {
+                System.out.println("Student ID not found.");
+            } else {
+                System.out.print("Enter student grade: ");
+                grade = in.nextFloat();
+                studentIdGrades.put(id, grade);
+                System.out.println("Grade updated successfully.");
+            }
+
+            //calculate average and display it
+            totalGrades = 0;
+            for (int key : studentIdGrades.keySet()) {
+                grade = studentIdGrades.get(key);
+                totalGrades += grade;
+            }
+            average = (double) totalGrades / studentIdGrades.size();
+            System.out.println("Average grade: "+average);
+
+            //classify performance based on average grade
+            if (average < 60) classification="Needs Improvement";
+            else if (average <=84) classification="Good Performance";
+            else classification="Excellent Performance";
+
+            //display all results
+            System.out.println("\nInformation -----\nTotal employee records entered: "+size);
+            System.out.println("Total unique students records: "+studentIdGrades.size());
+            System.out.println("All student  (ID=grade): "+studentIdGrades);
+            System.out.println("Average grade: "+average);
+            System.out.println("Class performance classification: "+classification);
 
         }
 
---
+++ java-352-363/src/main/java/task358/ProductPriceCatalog.java
@@ -0,0 +1,4 @@
+package task358;
+
+public class ProductPriceCatalog {
+}
